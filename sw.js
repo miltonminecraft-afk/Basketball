6dbf575b08dcfda21112321ef31c92cccd775a74
miltonminecraft-afk <milton.minecraft@gmail.com>
2026-09-13 18:33:16 +0200

    Refresh cache for results feed and attendance counts

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='basketball-agenda-v14';
-const SHELL=['./','./index.html','./styles.css?v=3','./club.css?v=2','./all-teams.js?v=5','./app.js?v=3','./club.js?v=3','./club-members-admin.js?v=5','./argon-ui.js?v=3','./manifest.webmanifest','./data/tasks.json'];
+const CACHE='basketball-agenda-v15';
+const SHELL=['./','./index.html','./styles.css?v=3','./club.css?v=2','./all-teams.js?v=5','./app.js?v=3','./club.js?v=3','./club-members-admin.js?v=5','./feed.js?v=1','./argon-ui.js?v=4','./manifest.webmanifest','./data/tasks.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
