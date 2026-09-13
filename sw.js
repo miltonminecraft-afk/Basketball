e2991bc2064fe875877976278b965ab6eaf5366c
miltonminecraft-afk <milton.minecraft@gmail.com>
2026-09-13 11:29:50 +0200

    Cache all-teams support

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='basketball-agenda-v3';
-const SHELL=['./','./index.html','./styles.css?v=3','./app.js?v=3','./manifest.webmanifest','./data/tasks.json'];
+const CACHE='basketball-agenda-v4';
+const SHELL=['./','./index.html','./styles.css?v=3','./all-teams.js?v=4','./app.js?v=3','./manifest.webmanifest','./data/tasks.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
